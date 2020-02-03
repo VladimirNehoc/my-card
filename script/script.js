643e33f5ccec34252ad042c1c7e4b253f7fb5562
Vladimir Nehoc <VladimirNehoc@mail.ru>
2020-02-03 17:03:08 +0400

    new symbols

--- script/script.js
+++ script/script.js
@@ -26,13 +26,6 @@ $(function() {
         if (event.target != this) return;
         callOut();
     });
-
-    //Проверка ширины окна для позиционирования символов
-    symbolCoords();
-
-    $(window).resize(function() {
-        symbolCoords();
-    });
 });
 
 function menuOpen() {
@@ -61,102 +54,6 @@ function callIn() {
 
 function callOut() {
     $('.back-call').fadeOut(200);
-}
-
-function symbolCoords() {
-    if ($(window).width() > 1366) {
-        $('.svg-animate-text-1').attr('x', '550');
-        $('.svg-animate-text-1').attr('y', '80');
-
-        $('.svg-animate-text-2').attr('x', '600');
-        $('.svg-animate-text-2').attr('y', '450');
-
-        $('.svg-animate-text-3').attr('x', '110');
-        $('.svg-animate-text-3').attr('y', '260');
-
-        $('.svg-animate-text-4').attr('x', '250');
-        $('.svg-animate-text-4').attr('y', '520');
-
-        $('.svg-animate-text-5').attr('x', '70');
-        $('.svg-animate-text-5').attr('y', '40');
-    }
-    if ($(window).width() <= 1366 && $(window).width() > 1024) {
-        $('.svg-animate-text-1').attr('x', '550');
-        $('.svg-animate-text-1').attr('y', '80');
-
-        $('.svg-animate-text-2').attr('x', '500');
-        $('.svg-animate-text-2').attr('y', '450');
-
-        $('.svg-animate-text-3').attr('x', '110');
-        $('.svg-animate-text-3').attr('y', '260');
-
-        $('.svg-animate-text-4').attr('x', '250');
-        $('.svg-animate-text-4').attr('y', '520');
-
-        $('.svg-animate-text-5').attr('x', '70');
-        $('.svg-animate-text-5').attr('y', '40');
-    }
-    if ($(window).width() <= 1024 && $(window).width() > 850) {
-        $('.svg-animate-text-1').attr('x', '550');
-        $('.svg-animate-text-1').attr('y', '40');
-
-        $('.svg-animate-text-2').attr('x', '400');
-        $('.svg-animate-text-2').attr('y', '430');
-
-        $('.svg-animate-text-3').attr('x', '110');
-        $('.svg-animate-text-3').attr('y', '260');
-
-        $('.svg-animate-text-4').attr('x', '250');
-        $('.svg-animate-text-4').attr('y', '520');
-
-        $('.svg-animate-text-5').attr('x', '70');
-        $('.svg-animate-text-5').attr('y', '40');
-    }
-    if ($(window).width() <= 850 && $(window).width() > 767) {
-        $('.svg-animate-text-1').attr('x', '550');
-        $('.svg-animate-text-1').attr('y', '40');
-
-        $('.svg-animate-text-2').attr('x', '320');
-        $('.svg-animate-text-2').attr('y', '430');
-
-        $('.svg-animate-text-3').attr('x', '110');
-        $('.svg-animate-text-3').attr('y', '260');
-
-        $('.svg-animate-text-4').attr('x', '250');
-        $('.svg-animate-text-4').attr('y', '520');
-    }
-    if ($(window).width() <= 767 && $(window).width() > 549) {
-        $('.svg-animate-text-1').attr('x', '450');
-        $('.svg-animate-text-1').attr('y', '40');
-
-        $('.svg-animate-text-2').attr('x', '240');
-        $('.svg-animate-text-2').attr('y', '460');
-
-        $('.svg-animate-text-3').attr('x', '210');
-        $('.svg-animate-text-3').attr('y', '120');
-
-        $('.svg-animate-text-4').attr('x', '50');
-        $('.svg-animate-text-4').attr('y', '520');
-
-        $('.svg-animate-text-5').attr('x', '70');
-        $('.svg-animate-text-5').attr('y', '40');
-    }
-    if ($(window).width() <= 549) {
-        $('.svg-animate-text-1').attr('x', '450');
-        $('.svg-animate-text-1').attr('y', '50');
-
-        $('.svg-animate-text-2').attr('x', '160');
-        $('.svg-animate-text-2').attr('y', '320');
-
-        $('.svg-animate-text-3').attr('x', '10');
-        $('.svg-animate-text-3').attr('y', '120');
-
-        $('.svg-animate-text-4').attr('x', '50');
-        $('.svg-animate-text-4').attr('y', '520');
-
-        $('.svg-animate-text-5').attr('x', '310');
-        $('.svg-animate-text-5').attr('y', '-70');
-    }
 }
 
 
